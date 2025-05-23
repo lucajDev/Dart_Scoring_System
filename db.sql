510a33563086dfc3627817bb4f066d6ee7a02181
Luca <luca.joe@web.de>
2025-05-23 18:39:50 +0200

    fixed a bug where databases would throw an error when creating via cmd/powershell

--- db.sql
+++ db.sql
@@ -1,29 +1,31 @@
--- Players-List 
+-- Players-List
+CREATE SEQUENCE IF NOT EXISTS players_id_seq; -- fixes bug if no database exists
 CREATE TABLE IF NOT EXISTS players
 (
     id integer NOT NULL DEFAULT nextval('players_id_seq'::regclass),
     name text COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT players_pkey PRIMARY KEY (id),
     CONSTRAINT players_name_key UNIQUE (name)
-);
+    );
 
 -- Statistic: Multiplayer (501 & 301)
 CREATE TABLE IF NOT EXISTS game_stats(
-    player_id integer NOT NULL,
-    games_played integer DEFAULT 0,
-    wins integer DEFAULT 0,
-    losses integer DEFAULT 0,
-    highest_checkout integer DEFAULT 0,
-    highest_9dart_avg numeric DEFAULT 0,
-    player_name text COLLATE pg_catalog."default",
-    CONSTRAINT game_stats_pkey PRIMARY KEY (player_id),
+                                         player_id integer NOT NULL,
+                                         games_played integer DEFAULT 0,
+                                         wins integer DEFAULT 0,
+                                         losses integer DEFAULT 0,
+                                         highest_checkout integer DEFAULT 0,
+                                         highest_9dart_avg numeric DEFAULT 0,
+                                         player_name text COLLATE pg_catalog."default",
+                                         CONSTRAINT game_stats_pkey PRIMARY KEY (player_id),
     CONSTRAINT game_stats_player_id_fkey FOREIGN KEY (player_id)
-        REFERENCES public.players (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-);
+    REFERENCES public.players (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    );
 
 -- Training: Around the clock singles
+CREATE SEQUENCE IF NOT EXISTS training_singles_id_seq; -- fixes bug if no database exists
 CREATE TABLE IF NOT EXISTS training_singles
 (
     id integer NOT NULL DEFAULT nextval('training_singles_id_seq'::regclass),
@@ -50,10 +52,11 @@ CREATE TABLE IF NOT EXISTS training_singles
     tries_target_20 integer,
     average_tries numeric,
     CONSTRAINT training_singles_pkey PRIMARY KEY (id)
-);
+    );
 
 
 -- Training: Around the clock doubles
+CREATE SEQUENCE IF NOT EXISTS training_doubles_id_seq; -- fixes bug if no database exists
 CREATE TABLE IF NOT EXISTS training_doubles
 (
     id integer NOT NULL DEFAULT nextval('training_doubles_id_seq'::regclass),
@@ -80,5 +83,5 @@ CREATE TABLE IF NOT EXISTS training_doubles
     tries_target_20 integer,
     average_tries numeric,
     CONSTRAINT training_doubles_pkey PRIMARY KEY (id)
-);
+    );
 
